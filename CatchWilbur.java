1565ef95bd9d7dece9d6525ba7d018683924ecc8
Tulsi <13208889+KuberDown1@users.noreply.github.com>
2018-01-16 01:27:14 -0700

    Create CatchWilbur.java

---
+++ CatchWilbur.java
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------------------
+// Extra Credit Assignment
+// Design and implement an application that plays a game called catch Wilbur
+// By Tulsi Kuber
+//--------------------------------------------------------------------------
+
+// driver class
+package CatchWilbur;
+
+import javax.swing.JFrame;
+public class CatchWilbur {
+ // Displays the main frame of the program.
+ public static void main(String[] args) {
+
+  JFrame frame = new JFrame("Catch Wilbur");
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  frame.getContentPane().add(new CatchWilburPanel());
+  frame.pack();
+  frame.setVisible(true);
+ }
+}
